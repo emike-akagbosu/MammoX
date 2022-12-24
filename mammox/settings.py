0c83c2fda187708bfca63fb2a422dc0e38834dea
sonya-k1 <sonya_kalsi@hotmail.co.uk>
2022-12-24 15:32:31 +0000

    added heroku allowed host

--- mammox/settings.py
+++ mammox/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = 'django-insecure-z706ln0j23lm73moh!gso_y2#d$94y$h05_0mmhihh%d+69#e1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['nameless-fjord-35005.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1',
+'mammox.herokuapp.com']
 
 
 # Application definition
